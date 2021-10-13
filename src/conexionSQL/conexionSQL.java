9da40f9463fca9083223bc0d4063dd43bdd3c8dd
Pc1234557 <pmeridac@miumg.edu.gt>
2021-10-13 01:35:14 -0600

    Formulario Usuarios,Login,Main

---
+++ src/conexionSQL/conexionSQL.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Pcortez
+ */
+public class conexionSQL {
+        
+    Connection conectar=null;
+     
+    public Connection conexion() {
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            /*estructura (server,nombre base de datos, usuario, contraseña)*/
+            conectar=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/proyecto","root","root");
+            
+            /*JOptionPane.showMessageDialog(null,"Conexion Exitosa");*/
+            
+    } catch(Exception e){
+        JOptionPane.showMessageDialog(null,"Conexion Fallida" + e.getMessage());
+    }
+        return conectar;
+    }
+}
